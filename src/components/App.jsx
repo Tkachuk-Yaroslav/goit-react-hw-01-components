7f875bee73fefed8181f3c45c8c19418a8d9466d
Tkachuk-Yaroslav <tkachuky105@gmail.com>
2023-08-29 17:19:34 +0300

    add Profile and App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import Profile from "./Profile";
+import user from '../data-json/user';
+
+
+
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+      // style={{
+      //   height: '100vh',
+      //   display: 'flex',
+      //   justifyContent: 'center',
+      //   alignItems: 'center',
+      //   fontSize: 40,
+      //   color: '#010101'
+      // }}
     >
-      React homework template
+      {/* React homework template. */}
+     <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
+// import Profile from './components/Profile';
+
+//<Profile
+// username={user.username}
+// tag={user.tag}
+//  location={user.location}
+//  avatar={user.avatar}
+// stats={user.stats}
+// />;
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
